ed36c326847275f4c93c90b2f77b399ac8344cfd
Nandini Banerjee <nandinibanerjee@yahoo.com>
2016-10-25 11:42:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/Cisco_Logo_no_TM_Cisco_Blue-RGB_31px.png') {
-      myImage.setAttribute ('src','images/Cisco_Logo_no_TM_White-RGB_31px.png');
+	if(mySrc === 'images/Cisco_Blue-RGB_31px.png') {
+      myImage.setAttribute ('src','Blue_Duotone_Gradient-RGB_31px.png');
 	} else {
-	  myImage.setAttribute ('src','images/Cisco_Logo_no_TM_Cisco_Blue-RGB_31px.png');
+	  myImage.setAttribute ('src','images/Cisco_Blue-RGB_31px.png');
 	}
 }
 
@@ -19,14 +19,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'JS is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'JS is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
